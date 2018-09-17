d8b84a6078656e7cfd966cafbd5e85496219db3a
TITONIChen <jj402517809@icloud.com>
2018-09-17 21:19:44 +0800

    0917

--- hello.py
+++ hello.py
@@ -1,7 +1,12 @@
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-m = numbers[3:6][4, 5, 6]
-n = numbers[0:1][1]
+#m = numbers[3:6][4, 5, 6]
+#n = numbers[0:1][1]
 
-print(m)
-print(n)
+#print(m)
+#print(n)
+
+users = ['TOM', 'Fiora', 'Sejuani']
+name = input("input your name: ")
+
+if name in users: print("True")
